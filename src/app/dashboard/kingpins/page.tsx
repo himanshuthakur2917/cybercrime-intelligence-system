91b83c346ee647dc2e7098807d525ff43b530734
ShwetaKumari-programming <swetakumari108sweta@gmail.com>
2026-01-12 17:19:25 +0530

    feat(dashboard) : add Kingpins Leaderboard

--- src/app/dashboard/kingpins/page.tsx
+++ src/app/dashboard/kingpins/page.tsx
@@ -2,10 +2,11 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>
-      {/* Kingpins leader board page goes here */}
-    </div>
-  )
+    // <div>
+      // {/* Kingpins leader board page goes here */}
+    // </div>
+    
+  );
 }
 
 export default page
